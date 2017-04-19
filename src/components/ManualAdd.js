446a6b4037fbde176e0b080abefe39709ae74930
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-19 13:30:57 -0700

    Finished manual add feature/functionality

--- src/components/ManualAdd.js
+++ src/components/ManualAdd.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
+import { connect } from 'react-redux';
+import { addFoodItem } from '../actions';
 
 class ManualAdd extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      showModal: false,
+      name: '',
+      calories: '',
+      quantity: ''
     }
 
     this.handleOpenModal = this.handleOpenModal.bind(this);
@@ -20,6 +25,19 @@ class ManualAdd extends Component {
     this.setState({showModal: false});
   }
 
+  addItem(name, calories, quantity) {
+    const item = {
+      name: name,
+      calories: calories,
+      totalCalories: calories * quantity,
+      quantity: quantity,
+      id: Math.random()
+    }
+    this.props.addFoodItem(item);
+    this.handleCloseModal();
+    this.setState({ name: '', calories: '', quantity: ''});
+  }
+
   render() {
     return (
       <div>
@@ -32,7 +50,47 @@ class ManualAdd extends Component {
           Add Food
         </button>
         <ReactModal isOpen={this.state.showModal} contentLabel="Add Food Manually">
-          <p>test</p>
+          <h4>Please enter food name, calories per serving, and number of servings</h4>
+          <div>
+            <span><strong>Name: </strong></span>
+            <input
+              className="form-control"
+              onChange={event => this.setState({name: event.target.value})}
+            />
+          </div>
+          <div>
+            <span><strong>Calories Per Serving: </strong></span>
+            <input
+              className="form-control"
+              onChange={event => this.setState({calories: event.target.value})}
+            />
+          </div>
+          <div>
+            <span><strong>Servings: </strong></span>
+            <select name="quantity"
+              onChange={event => this.setState({quantity: event.target.value})}
+              defaultValue="0"
+            >
+              <option value={0}>-</option>
+              <option value={1}>01</option>
+              <option value={2}>02</option>
+              <option value={3}>03</option>
+              <option value={4}>04</option>
+              <option value={5}>05</option>
+              <option value={6}>06</option>
+              <option value={7}>07</option>
+              <option value={8}>08</option>
+              <option value={9}>09</option>
+              <option value={10}>10</option>
+            </select>
+          </div>
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={() => this.addItem(this.state.name, this.state.calories, this.state.quantity)}
+          >
+            Add Item
+          </button>
           <button
             className="btn btn-danger"
             type="button"
@@ -46,4 +104,4 @@ class ManualAdd extends Component {
   }
 }
 
-export default ManualAdd;
+export default connect(null, { addFoodItem })(ManualAdd);
