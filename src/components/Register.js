4329aaa54d77f56d82c37fe41c8e0a0755288b50
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-26 09:18:57 -0700

    styled registration page

--- src/components/Register.js
+++ src/components/Register.js
@@ -30,33 +30,35 @@ class Register extends Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        <h2>Register</h2>
-        <p>To use this app, please sign up using a correctly formatted email, and a password.</p>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            className="form-control"
-            placeholder="email"
-            onChange={event => this.setState({email: event.target.value})}
-          />
-          <label>Password:</label>
-          <input
-            className="form-control"
-            type="password"
-            placeholder="password"
-            onChange={event => this.setState({password: event.target.value})}
-          />
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={() => this.register()}
-          >
-            Register
-          </button>
+      <div className="Register">
+        <div className="register-content">
+          <h2>Register</h2>
+          <p>To use this app, please sign up using a correctly formatted email, and a password.</p>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              className="form-control register-input register-input-field"
+              placeholder="email"
+              onChange={event => this.setState({email: event.target.value})}
+            />
+            <label>Password:</label>
+            <input
+              className="form-control register-input register-input-field"
+              type="password"
+              placeholder="password"
+              onChange={event => this.setState({password: event.target.value})}
+            />
+            <button
+              className="btn btn-primary register-input"
+              type="button"
+              onClick={() => this.register()}
+            >
+              Register
+            </button>
+          </div>
+          <div>{this.state.error.message}</div>
+          <div><Link to={'/'}>Already a user? Sign in instead</Link></div>
         </div>
-        <div>{this.state.error.message}</div>
-        <div><Link to={'/'}>Already a user? Sign in instead</Link></div>
       </div>
     )
   }
