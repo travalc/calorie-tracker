065c7238a7db5abd4238b1fa47d826d3878143c1
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-26 14:03:45 -0700

    Started styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,21 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="about" style={{padding: '20px'}}>
-        <h2>Welcome Back!</h2>
-        <h4>What would you like to do?</h4>
+      <div className="Home">
+        <div className="home-head">
+          <h2>Welcome Back!</h2>
+          <h4>What would you like to do?</h4>
+        </div>
+        <div className="box-container">
+          <div className="box-button">
+            <span>Add Foods For Today</span>
+            <span className="fa fa-plus button-icon"></span>
+          </div>
+          <div className="box-button">
+            <span>View Your Progress/Stats</span>
+            <span className="fa fa-line-chart button-icon"></span>
+          </div>
+        </div>
       </div>
     )
   }
